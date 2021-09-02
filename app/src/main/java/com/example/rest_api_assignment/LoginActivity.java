81ecf064f0783ad2ceb3c4c92f08444837f9167f
Connor Turetzky <cturetzky@gmail.com>
2021-09-02 13:57:22 -0700

    Added comments on view ids for code logic

--- app/src/main/java/com/example/rest_api_assignment/LoginActivity.java
+++ app/src/main/java/com/example/rest_api_assignment/LoginActivity.java
@@ -1,4 +1,9 @@
 package com.example.rest_api_assignment;
 
 public class LoginActivity {
+    // Username input id: username_input
+    // Password input id: password_input
+    // Login button id: login_button
+    // Simple logic, check username against database, if found check password against database. Send
+    // to MainActivity if check successful. Display error otherwise.
 }
