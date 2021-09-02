ee4ac04656f6010cc7acc19ac015d7c62fcaa59c
Connor Turetzky <cturetzky@gmail.com>
2021-09-01 21:00:03 -0700

    Initial commit

---
+++ app/src/main/java/com/example/rest_api_assignment/JsonPlaceHolderApi.java
@@ -0,0 +1,14 @@
+package com.example.rest_api_assignment;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface JsonPlaceHolderApi {
+
+    @GET("posts")
+    Call <List<Post>> getPosts();
+}
---
+++ app/src/main/java/com/example/rest_api_assignment/Post.java
@@ -0,0 +1,25 @@
+package com.example.rest_api_assignment;
+
+public class Post {
+
+    private int userId;
+    private int id;
+    private String body;
+    private String title;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
