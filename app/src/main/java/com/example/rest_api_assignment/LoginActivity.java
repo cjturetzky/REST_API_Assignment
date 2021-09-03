1c2ecaea3b0650b3a8f42e16cb477adc310b8716
Connor Turetzky <cturetzky@gmail.com>
2021-09-03 13:52:08 -0700

    Set login page to default opening screen

--- app/src/main/java/com/example/rest_api_assignment/LoginActivity.java
+++ app/src/main/java/com/example/rest_api_assignment/LoginActivity.java
@@ -1,9 +1,21 @@
 package com.example.rest_api_assignment;
 
-public class LoginActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
     // Username input id: username_input
     // Password input id: password_input
     // Login button id: login_button
     // Simple logic, check username against database, if found check password against database. Send
     // to MainActivity if check successful. Display error otherwise.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
 }
