e85355a976fd5494812b8ec6631eebf47062edf5
Connor Turetzky <cturetzky@gmail.com>
2021-09-03 14:03:44 -0700

    Pull username and password strings from application for checking

--- app/src/main/java/com/example/rest_api_assignment/LoginActivity.java
+++ app/src/main/java/com/example/rest_api_assignment/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.rest_api_assignment;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,13 +13,22 @@ public class LoginActivity extends AppCompatActivity {
     // Simple logic, check username against database, if found check password against database. Send
     // to MainActivity if check successful. Display error otherwise.
 
+    EditText usernameEdit;
+    EditText passwordEdit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        usernameEdit = (EditText)findViewById(R.id.username_input);
+        passwordEdit = (EditText)findViewById(R.id.password_input);
     }
 
     public void checkLogin(View view){
+        String username = usernameEdit.getText().toString();
+        String password = passwordEdit.getText().toString();
 
+        System.out.println("Username: " + username + "\nPassword: " + password);
     }
 }
